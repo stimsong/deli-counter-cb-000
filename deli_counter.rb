3f909e670cf9d835c9920d3cc0b283c0f2bfbdc8
Greg Stimson <stimsong@yahoo.com>
2018-03-14 22:03:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,12 @@
 katz_deli = []
 
 def line(katz_deli)
-  current_line = []
-  if katz_deli.length > 0
 
+  if katz_deli.length > 0
+    current_line = "The line is currently:"
+    katz_deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
   else
     puts "The line is currently empty."
   end
